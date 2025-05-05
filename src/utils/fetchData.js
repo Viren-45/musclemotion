21bae660cc6de3e687b28f3b6b5c75603a4c2fcc
culixo <culixo.official@gmail.com>
2025-05-05 15:41:48 -0400

    Full functional webapp

---
+++ src/utils/fetchData.js
@@ -0,0 +1,22 @@
+export const exerciseOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'exercisedb.p.rapidapi.com',
+      'X-RapidAPI-Key': 'd567bbac13msh0fbab99b37e545dp1cd387jsn71e6d0e08ba9',
+    },
+  };
+  
+  export const youtubeOptions = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Host': 'youtube-search-and-download.p.rapidapi.com',
+      'X-RapidAPI-Key': 'd567bbac13msh0fbab99b37e545dp1cd387jsn71e6d0e08ba9',
+    },
+  };
+  
+  export const fetchData = async (url, options) => {
+    const res = await fetch(url, options);
+    const data = await res.json();
+  
+    return data;
+  };
